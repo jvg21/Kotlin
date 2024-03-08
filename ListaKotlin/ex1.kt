f8e16850280fda63b97d54a0edbaf5ab77e2b884
João Gregorini <75985123+jvg21@users.noreply.github.com>
2024-03-08 13:20:34 -0300

    Update ex1.kt

--- ListaKotlin/ex1.kt
+++ ListaKotlin/ex1.kt
@@ -1,1 +1,9 @@
+// 1. Crie uma função que receba um número inteiro e retorne verdadeiro se o número for par e falso caso contrário. 
+fun verificaPar(number:Int):Boolean{
+    return number % 2 == 0;
+}
 
+fun main() {
+    val number = 6
+    println(verificaPar(number))
+}
