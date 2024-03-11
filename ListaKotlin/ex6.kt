7267796d4aa2232836995c355f9c536a86415a5d
João Gregorini <75985123+jvg21@users.noreply.github.com>
2024-03-11 11:30:21 -0300

    Update ex6.kt

--- ListaKotlin/ex6.kt
+++ ListaKotlin/ex6.kt
@@ -1,1 +1,22 @@
+// 6. Crie uma classe "ContaBancaria" com os atributos "saldo" e "limite". Adicione um método chamado "saque" que recebe um valor como parâmetro e subtrai do saldo da conta. Se o valor do saque for maior que o saldo da conta, o método deve lançar uma exceção com a mensagem "Saldo insuficiente". 
 
+class ContaBancaria(var saldo:Double, var limite:Double){
+    
+    fun saque(valor:Double){
+        if (valor > saldo + limite) {
+            throw IllegalArgumentException("Saldo insuficiente")
+        } else {
+            saldo -= valor
+        }
+    }
+}
+fun main() {
+	val ContaBancaria = ContaBancaria(1000.0,1000.0)
+    println(ContaBancaria.saldo)
+ 	try{
+        ContaBancaria.saque(200.0);
+        println(ContaBancaria.saldo)
+    }catch(e: IllegalArgumentException){
+        println(e)
+    }
+}
